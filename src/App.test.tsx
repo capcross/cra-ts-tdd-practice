bc149335e9b676513b3fd2c23942b85af510cd9c
AlexanderLapygin <AlexanderLapygin@gmail.com>
2018-12-02 20:04:35 +0300

    feat: create project

---
+++ src/App.test.tsx
@@ -0,0 +1,21 @@
+import { shallow } from 'enzyme';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import App, { Heading } from './App';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App/>, div);
+    ReactDOM.unmountComponentAtNode(div);
+});
+
+it('renders the heading', () => {
+    const wrapper = shallow(<App/>);
+    expect(wrapper.find('h1').text()).toBe('Hello React');
+});
+
+it('renders the heading', () => {
+	const wrapper = shallow(<Heading/>);
+	expect(wrapper.find('h1').text())
+			.toBe('Hello React');
+});
---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export const Heading = () => <h1>Hello React</h1>;
+
+class App extends React.Component {
+	public render() {
+			return (
+					<div>
+							<h1>Hello React</h1>
+					</div>
+			);
+	}
+}
+
+export default App;
